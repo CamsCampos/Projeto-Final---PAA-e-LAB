0d1c97dfc24ccfb811490f16aa8533c338646fb5
Camila Silva Campos <camilasilvacampos2013@gmail.com>
2020-06-13 19:37:35 -0300

    Alterações no menu e na classe SubsequenciaCresMaxima

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,17 @@
 import java.util.ArrayList;
 import java.util.InputMismatchException;
+import java.util.Random;
 import java.util.Scanner;
 
+/**
+ * 
+ * @author Camila Campos, ... (coloque seu nome)
+ *
+ */
 public class Main {
 
 	static Scanner sc = new Scanner(System.in);
+	static Random rd = new Random();
 
 	public static void main(String[] args) {
 
@@ -17,12 +24,15 @@ public class Main {
 	 */
 	public static void menu() {
 
-		int opcao = 0;
+		int opcaoMenu = 0;
+
 		do {
 			System.out.println("\n\n\n\n               =========== MENU ============\n");
-			System.out.println("     | Subsequência Crescente Máxima: ");
-			System.out.println("     |    1 - Força bruta ");
-			System.out.println("     |    2 - Programação dinâmica ");
+			System.out.println("     | OBS: O número entre parênteses indica a quantidade \n       "
+					+ "máxima de elementos em um conjunto para executar em até 5 segundos: ");
+			System.out.println("\n     | Subsequência Crescente Máxima: ");
+			System.out.println("     |    1 - Força bruta (75, sujeito à grandes variações) ");
+			System.out.println("     |    2 - Programação dinâmica (25.000) ");
 			System.out.println("\n     | (Nome do seu algoritmo): ");
 			System.out.println("     |    3 - XXX ");
 			System.out.println("     |    4 - YYY ");
@@ -35,18 +45,15 @@ public class Main {
 			System.out.println("\n     | 0 - Sair ");
 
 			System.out.print("\n Digite sua opção: ");
-			opcao = sc.nextInt();
+			opcaoMenu = sc.nextInt();
 			System.out.print("\n");
-			switch (opcao) {
+			switch (opcaoMenu) {
 			case 1:
-				System.out.println("\n ==> Subsequência Crescente Máxima (Força Bruta): ");
-				System.out.println("\n     -> Subsequência(s) Crescente(s) Máxima(s) (Força Bruta) = "
-						+ SubsequenciaCrescMaxima.forcaBruta(leSequencia()));
+				iniciaSubseqCresMáxima("Força Bruta");
 				break;
 			case 2:
-				System.out.println("\n ==> Subsequência Crescente Máxima (Programação Dinâmica): ");
-				System.out.println("\n     -> Subsequência(s) Crescente(s) Máxima(s) (Programação Dinâmica) = "
-						+ SubsequenciaCrescMaxima.programacaoDinamica(leSequencia()));
+				iniciaSubseqCresMáxima("Programação Dinâmica");
+
 				break;
 			case 3:
 				System.out.println("\n ==> (Nome do seu algoritmo): ");
@@ -54,23 +61,92 @@ public class Main {
 			case 4:
 				System.out.println("\n ==> (Nome do seu algoritmo): ");
 				break;
+			case 5:
+				System.out.println("\n ==> (Nome do seu algoritmo): ");
+				break;
+			case 6:
+				System.out.println("\n ==> (Nome do seu algoritmo): ");
+				break;
+			case 7:
+				System.out.println("\n ==> (Nome do seu algoritmo): ");
+				break;
+			case 8:
+				System.out.println("\n ==> (Nome do seu algoritmo): ");
+				break;
 			case 0:
 				break;
 			default:
-				System.out.println("Opção Inválida!");
+				System.out.println("\n  >>> Opção Inválida! <<<\n");
 				break;
 			}
-		} while (opcao != 0);
+		} while (opcaoMenu != 0);
 		sc.close();
 		System.out.println("\n\n FIM DO PROGRAMA! ");
 	}
 
-	/**
-	 * @brief Lê subsequência de inteiros do teclado
-	 *
-	 * @return Lista com a subsequência de inteiros
-	 */
-	public static ArrayList<Integer> leSequencia() {
+	public static void iniciaSubseqCresMáxima(String tipoAlgoritmo) {
+		int opcaoEntrada;
+		long tempoGasto;
+		ArrayList<Integer> conjunto = new ArrayList<Integer>();
+		ArrayList<ArrayList<Integer>> resultado = new ArrayList<ArrayList<Integer>>();
+		boolean repetir;
+
+		do {
+			System.out.println("\n ==> Subsequência Crescente Máxima (" + tipoAlgoritmo + "): ");
+			System.out.println("\n   -> Informe sua opção de entrada dos dados:");
+			System.out.println("\n	1- Gerar conjunto aleatório: ");
+			System.out.println("\n	2- Inserir conjunto: ");
+			opcaoEntrada = sc.nextInt();
+
+			repetir = false;
+			switch (opcaoEntrada) {
+			case 1:
+				System.out.println("\n   -> Qual o tamanho do seu conjunto? ");
+				conjunto = gerarConjunto(sc.nextInt());
+				System.out.println("\n   -> Conjunto gerado: " + conjunto);
+
+				break;
+			case 2:
+				conjunto = lerConjunto();
+				System.out.println("\n   -> Conjunto inserido: " + conjunto);
+
+				break;
+			default:
+				System.out.println("\n  >>> Opção Inválida! <<<\n");
+				repetir = true;
+				break;
+			}
+		} while (repetir);
+
+		switch (tipoAlgoritmo) {
+		case "Força Bruta":
+			tempoGasto = System.currentTimeMillis();
+			resultado = SubsequenciaCrescMaxima.forcaBruta(conjunto);
+			System.out.println("\n   -> Tempo gasto = " + (System.currentTimeMillis() - tempoGasto) + " milissegundos");
+			System.out.println("   ->  Resultado: " + resultado);
+			break;
+
+		case "Programação Dinâmica":
+			tempoGasto = System.currentTimeMillis();
+			resultado = SubsequenciaCrescMaxima.programacaoDinamica(conjunto);
+			System.out.println("\n   -> Tempo gasto = " + (System.currentTimeMillis() - tempoGasto) + " milissegundos");
+			System.out.println("   ->  Resultado: " + resultado);
+			break;
+		default:
+			System.out.println("\n  >>> Opção Inválida! <<<\n");
+			repetir = true;
+		}
+	}
+
+	public static ArrayList<Integer> gerarConjunto(int tamanho) {
+		ArrayList<Integer> conjunto = new ArrayList<Integer>();
+		for (int i = 0; i < tamanho; i++) {
+			conjunto.add(rd.nextInt(100));
+		}
+		return conjunto;
+	}
+
+	public static ArrayList<Integer> lerConjunto() {
 		int num = 0;
 		ArrayList<Integer> sequencia = new ArrayList<Integer>();
 
@@ -91,6 +167,7 @@ public class Main {
 				sc.nextLine();
 			}
 		}
+
 		return sequencia;
 
 	}
--- src/SubsequenciaCrescMaxima.java
+++ src/SubsequenciaCrescMaxima.java
@@ -4,8 +4,54 @@ import java.util.ArrayList;
  * @brief Resolve o problema de Subsequência Crescente Máxima
  *
  * @author Camila Silva Campos
+ * 
  */
 public class SubsequenciaCrescMaxima {
+
+	/**
+	 * @brief Subsequência crescrente máxima com força bruta
+	 *
+	 * @param seq Sequência de inteiros.
+	 *
+	 * @return Lista com a(s) subsequência(s) crescente(s) máxima(s)
+	 */
+	@SuppressWarnings("unchecked")
+	public static ArrayList<ArrayList<Integer>> forcaBruta(ArrayList<Integer> seq) {
+
+		ArrayList<ArrayList<Integer>> subconjuntos = new ArrayList<ArrayList<Integer>>();
+		subconjuntos.add(new ArrayList<Integer>());
+
+		// Gera todos os subconjuntos da sequência de inteiros
+		while (!seq.isEmpty()) {
+			int tamSubConj = subconjuntos.size();
+			for (int i = 0; i < tamSubConj; i++) {
+
+				// Verificação para adicionar somente sequências crescentes à lista
+				// "subconjuntos"
+				if ((i == 0) || (seq.get(0) > subconjuntos.get(i).get(subconjuntos.get(i).size() - 1))) {
+					subconjuntos.add((ArrayList<Integer>) subconjuntos.get(i).clone());
+					subconjuntos.get(subconjuntos.size() - 1).add(seq.get(0));
+				}
+			}
+			seq.remove(0);
+		}
+
+		int tamMelhorSequencia = 0;
+		ArrayList<ArrayList<Integer>> subMaxima = new ArrayList<ArrayList<Integer>>();
+
+		// Busca a(s) subsequência(s) máxima(s) e adiciona à lista "subMax"
+		for (int i = 1; i < subconjuntos.size(); i++) {
+			if (subconjuntos.get(i).size() > tamMelhorSequencia) {
+				tamMelhorSequencia = subconjuntos.get(i).size();
+				subMaxima.clear();
+				subMaxima.add(subconjuntos.get(i));
+			} else if (subconjuntos.get(i).size() == tamMelhorSequencia) {
+				subMaxima.add(subconjuntos.get(i));
+			}
+		}
+
+		return subMaxima;
+	}
 
 	/**
 	 * @brief Subsequência crescrente máxima com programação dinâmica
@@ -21,33 +67,36 @@ public class SubsequenciaCrescMaxima {
 		int posMelhorCandidato = -1;
 		tabela.add(null);
 
-		// Percorre a sequência de números
+		// Percorre os elementos da sequência informada
 		for (int i = 0; i < seq.size(); i++) {
 
 			// Percorre a tabela dinâmica do fim para o ínicio
 			for (int j = tabela.size() - 1; j >= 0; j--) {
 
-				// Verifica se é uma sequência válida e compara com a melhor encontrada até o
-				// momento
+				// Verifica se é a sequência da posição atual da tabela é válida para adicionar
+				// o elemento atual
 				if (tabela.get(j) != null && tabela.get(j).get(tabela.get(j).size() - 1) < seq.get(i)) {
-					if (posMelhorCandidato == -1)
+
+					// Atualiza a variável, se necessário, da posição do melhor candidato até o
+					// momento para o elemento atual
+					if ((posMelhorCandidato == -1) || (tabela.get(j).size() > tabela.get(posMelhorCandidato).size())) {
 						posMelhorCandidato = j;
-					else if (tabela.get(j).size() > tabela.get(posMelhorCandidato).size())
-						posMelhorCandidato = j;
+					}
 				}
 			}
 
-			// Adiciona o número na melhor sequência encontrada
+			// Adiciona o elemento atual na melhor sequência encontrada
 			if (posMelhorCandidato != -1) {
 				tabela.add((ArrayList<Integer>) tabela.get(posMelhorCandidato).clone());
 				tabela.get(tabela.size() - 1).add(seq.get(i));
 			} else {
-				// Não há sequência para o número. Ele é adicionado sozinho à tabela
+				// Não há sequência para o elemento atual. Ele é adicionado sozinho à tabela
 				tabela.add(new ArrayList<Integer>());
 				tabela.get(tabela.size() - 1).add(seq.get(i));
 			}
 
-			posMelhorCandidato = -1; // Zera a posição do melhor candidato
+			posMelhorCandidato = -1; // Zera a posição do melhor candidato para reiniciar a tabela com o próximo
+										// elemento
 
 		}
 
@@ -65,53 +114,6 @@ public class SubsequenciaCrescMaxima {
 				subMaxima.add(tabela.get(i));
 			}
 		}
-		return subMaxima;
-	}
-
-	/**
-	 * @brief Subsequência crescrente máxima com força bruta
-	 *
-	 * @param seq Sequência de inteiros.
-	 *
-	 * @return Lista com a(s) subsequência(s) crescente(s) máxima(s)
-	 */
-	@SuppressWarnings("unchecked")
-	public static ArrayList<ArrayList<Integer>> forcaBruta(ArrayList<Integer> seq) {
-
-		ArrayList<ArrayList<Integer>> subconjuntos = new ArrayList<ArrayList<Integer>>();
-		subconjuntos.add(new ArrayList<Integer>());
-
-		// Gera todos os subconjuntos da sequencia de inteiros
-		while (!seq.isEmpty()) {
-			int tamSubConj = subconjuntos.size();
-			for (int i = 0; i < tamSubConj; i++) {
-
-				// Adiciona os subconjuntos crescentes à lista "subconjuntos"
-				if (i == 0) {
-					subconjuntos.add((ArrayList<Integer>) subconjuntos.get(i).clone());
-					subconjuntos.get(subconjuntos.size() - 1).add(seq.get(0));
-				} else if (seq.get(0) > subconjuntos.get(i).get(subconjuntos.get(i).size() - 1)) {
-					subconjuntos.add((ArrayList<Integer>) subconjuntos.get(i).clone());
-					subconjuntos.get(subconjuntos.size() - 1).add(seq.get(0));
-				}
-			}
-			seq.remove(0);
-		}
-
-		int tamMelhorSequencia = 0;
-		ArrayList<ArrayList<Integer>> subMaxima = new ArrayList<ArrayList<Integer>>();
-
-		// Busca a(s) subsequência(s) máxima(s)
-		for (int i = 1; i < subconjuntos.size(); i++) {
-			if (subconjuntos.get(i).size() > tamMelhorSequencia) {
-				tamMelhorSequencia = subconjuntos.get(i).size();
-				subMaxima.clear();
-				subMaxima.add(subconjuntos.get(i));
-			} else if (subconjuntos.get(i).size() == tamMelhorSequencia) {
-				subMaxima.add(subconjuntos.get(i));
-			}
-		}
-
 		return subMaxima;
 	}
 
